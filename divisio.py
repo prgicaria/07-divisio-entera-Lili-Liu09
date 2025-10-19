0dde30e3d4c8f5e40356f9c5b045d5fe6eb5ac8c
LLiu-icaria <lliu@instituticaria.cat>
2025-10-19 16:30:59 +0200

    correcció .2

--- divisio.py
+++ divisio.py
@@ -3,7 +3,8 @@ divisor = int(input("Quin és el divisor?"))
 
 quocient = divident//divisor
 residu = divident-(divisor*quocient)
+divisió = {divident}/{divisor}
 
-print(f"Divisió: {divident: .0f}/{divisor: .0f}")
+print(f"Divisió: {divisió: .0f}")
 print(f"Quocient: {quocient: .0f}")
 print(f"Residu: {residu: .0f}")
