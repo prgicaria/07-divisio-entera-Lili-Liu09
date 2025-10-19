d15ca99764f851be71f6f4d5b994a9c8d937c955
LLiu-icaria <lliu@instituticaria.cat>
2025-10-19 16:25:56 +0200

    correcció .1

--- divisio.py
+++ divisio.py
@@ -1,9 +1,9 @@
-divident = float(input("Quin és el divident?"))
-divisor = float(input("Quin és el divisor?"))
+divident = int(input("Quin és el divident?"))
+divisor = int(input("Quin és el divisor?"))
 
 quocient = divident//divisor
 residu = divident-(divisor*quocient)
 
-print(f"Divisió: {divident}/{divisor}")
-print(f"Quocient: {quocient}")
-print(f"Residu: {residu}")
+print(f"Divisió: {divident: .0f}/{divisor: .0f}")
+print(f"Quocient: {quocient: .0f}")
+print(f"Residu: {residu: .0f}")
