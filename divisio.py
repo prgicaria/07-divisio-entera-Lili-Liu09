2efe0eb56501d03e390668cbd4377799917a30b1
LLiu-icaria <lliu@instituticaria.cat>
2025-10-20 08:31:01 +0200

    correcció .10

--- divisio.py
+++ divisio.py
@@ -4,6 +4,6 @@ divisor = int(input())
 quocient = dividend//divisor
 residu = dividend-(divisor*quocient)
 
-print(f"Divisió:{dividend}/{divisor}")
+print(f"Divisió: {dividend}/{divisor}")
 print(f"Quocient: {quocient}")
 print(f"Residu: {residu}")
